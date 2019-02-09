37d36b934270e5e8263a9bdceaea5b5171419ee7
axetroy <troy450409405@gmail.com>
2019-02-09 18:22:49 +0800

    update

--- move.go
+++ move.go
@@ -1,50 +1,47 @@
 package fs
 
 import (
-  "os"
-  "io/ioutil"
-  "path"
+	"io/ioutil"
+	"os"
+	"path"
 )
 
-/*
-move a file or dir
- */
+// Move file/folder
 func Move(src string, target string) (err error) {
 
-  var (
-    fileInfo os.FileInfo
-    files    []os.FileInfo
-  )
+	var (
+		fileInfo os.FileInfo
+		files    []os.FileInfo
+	)
 
-  if fileInfo, err = os.Stat(src); err != nil {
-    return
-  }
+	if fileInfo, err = os.Stat(src); err != nil {
+		return
+	}
 
-  if fileInfo.IsDir() {
+	if fileInfo.IsDir() {
 
-    // read dir and move one by one
-    if files, err = ioutil.ReadDir(src); err != nil {
-      return
-    }
+		// read dir and move one by one
+		if files, err = ioutil.ReadDir(src); err != nil {
+			return
+		}
 
-    if err = EnsureDir(target); err != nil {
-      return
-    }
+		if err = EnsureDir(target); err != nil {
+			return
+		}
 
-    for _, file := range files {
-      filename := file.Name()
-      srcFile := path.Join(src, filename)
-      targetFile := path.Join(target, filename)
-      if err = Move(srcFile, targetFile); err != nil {
-        return err
-      }
-    }
+		for _, file := range files {
+			filename := file.Name()
+			srcFile := path.Join(src, filename)
+			targetFile := path.Join(target, filename)
+			if err = Move(srcFile, targetFile); err != nil {
+				return err
+			}
+		}
 
-    // copy all done, should remove the src dir
-    err = os.RemoveAll(src)
+		// copy all done, should remove the src dir
+		err = os.RemoveAll(src)
 
-    return
-  } else {
-    return os.Rename(src, target)
-  }
+		return
+	}
+	return os.Rename(src, target)
 }
